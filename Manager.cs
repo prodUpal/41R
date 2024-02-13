573a24b6bee62869ee136bfadacbd87813d07ade
prodUpal <tim.ism06@gmail.com>
2024-02-13 17:47:15 +0500

    17 laba

---
+++ Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ismagilov16_17
+{
+    internal class Manager
+    {
+        public static Frame MainFrame { get; set; }
+    }
+}
