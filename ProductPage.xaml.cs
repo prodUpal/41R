6ff175fac8a072487b3ab67ba7b56f0de2b324c8
prodUpal <tim.ism06@gmail.com>
2024-02-16 10:22:21 +0500

    18_________

--- ProductPage.xaml.cs
+++ ProductPage.xaml.cs
@@ -26,7 +26,76 @@ namespace ismagilov16_17
 
             var currentProduct = Ismagilov41Entities.GetContext().Product.ToList();
 
+            ProdAll.Text = Convert.ToString(currentProduct.Count);
+            ProdAtTheMoment.Text = Convert.ToString(currentProduct.Count);
+
             ProductListView.ItemsSource = currentProduct;
+        }
+
+
+        private void UpdateProduct()
+        {
+            var currentProduct = Ismagilov41Entities.GetContext().Product.ToList();
+
+            currentProduct = currentProduct.Where(p => p.ProductName.ToLower().Contains(TBSearch.Text.ToLower())).ToList();
+
+            if (SortCombo.SelectedIndex == 0)
+            {
+                currentProduct = currentProduct.Where(p => Int32.Parse(p.SkidkaDeystv) > 0 && Int32.Parse(p.SkidkaDeystv) <= 9.99).ToList();
+            }
+
+            if (SortCombo.SelectedIndex == 1)
+            {
+                currentProduct = currentProduct.Where(p => Int32.Parse(p.SkidkaDeystv) > 10 && Int32.Parse(p.SkidkaDeystv) <= 14.99).ToList();
+            }
+
+            if (SortCombo.SelectedIndex == 2)
+            {
+                currentProduct = currentProduct.Where(p => Int32.Parse(p.SkidkaDeystv) > 15).ToList();
+            }
+            if (SortCombo.SelectedIndex == 3)
+            {
+                currentProduct = currentProduct.Where(p => Int32.Parse(p.SkidkaDeystv) != null).ToList();
+
+            }
+
+            ProdAtTheMoment.Text = Convert.ToString(currentProduct.Count);
+
+            if (CostUp.IsChecked.Value)
+            {
+                currentProduct = currentProduct.OrderByDescending(p => p.ProductCost).ToList();
+            }
+            if (CostDown.IsChecked.Value)
+            {
+                currentProduct = currentProduct.OrderBy(p => p.ProductCost).ToList();
+            }
+
+            ProductListView.ItemsSource = currentProduct;
+        }
+
+
+
+        private void SortCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+        private void TBSearch_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+
+
+        private void CostUp_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+        private void CostDown_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProduct();
         }
     }
 }
+
